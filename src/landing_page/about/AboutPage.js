bf0c9b1e149792c928e375e7a470511b00244fe2
Shubham Vishwakarma <sv8112004@gmail.com>
2025-10-22 19:53:11 +0530

    Home Component Part 3 Done

---
+++ src/landing_page/about/AboutPage.js
@@ -0,0 +1,22 @@
+import Hero from './Hero';
+import Team from './Team';
+
+import OpenAccount from '../OpenAccount';
+import Footer from '../Footer';
+import Navbar from '../Navbar';
+ 
+function AboutPage() {
+    return ( 
+       <>
+       
+       <Navbar/>
+       <Hero/>
+       <Team/>
+       <OpenAccount/>
+       <Footer/>
+
+       </>
+     );
+}
+
+export default AboutPage;
