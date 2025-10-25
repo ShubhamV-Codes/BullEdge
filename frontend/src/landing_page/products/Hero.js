b188f9fc246e1f2b41ffda87491db9068217c6a4
Shubham Vishwakarma <sv8112004@gmail.com>
2025-10-25 23:07:12 +0530

    Product Section Done

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 function Hero() {
-    return (
-        <h1>Hero</h1>
-      );
+  return (
+    <div className="container border-bottom text-center mb-4">
+      <h1 className=" mt-4 "> BullEdge Products</h1>
+      <p className="mb-3 fs-4 ">
+        Sleek, modern, and intuitive trading platforms
+      </p>
+      <p className="mb-5">
+        
+        Check out our
+        <Link to="/" style={{ textDecoration: "none" }}>
+          investment offerings →
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Hero;
