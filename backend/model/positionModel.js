5890167b2309c9843fe8175adc94b428042143e3
Shubham Vishwakarma <sv8112004@gmail.com>
2025-11-07 10:52:56 +0530

    Thing done

--- backend/model/positionModel.js
+++ backend/model/positionModel.js
@@ -1,6 +1,6 @@
 const {model}=require("mongoose");
 const {PositionSchema}=require("../schemas/PositionSchema");
 
-const PositionModel= new model("position",PositionSchema);
+const PositionModel= new model("Position",PositionSchema);
    
 module.exports={PositionModel};
